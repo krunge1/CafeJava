9be1f1d74ac36a03fb37ab1ed71b2281e1ab4d5e
Kyle Runge <krunge1@hotmail.com>
2023-03-12 07:42:59 -0500

    Base variable uses project

--- CafeJava.java
+++ CafeJava.java
@@ -1,6 +1,57 @@
 public class CafeJava {
-    public static void main(Sring[] args){
+    public static void main(String[] args){
+        // APP VARIABLES
+        // Lines of text that will appear in the app.
+        String generalGreeting = "Welcome to Cafe Java, ";
+        String pendingMessage = ", your order will be ready shortly";
+        String readyMessage = ", your order is ready";
+        String displayTotalMessage = "Your total is $";
         
+        // Menu variables (add yours below)
+        double mochaPrice = 3.5;
+        double dripCoffee = 1.25;
+        double latte = 3.4;
+        double cappucino = 6.70;
+    
+        // Customer name variables (add yours below)
+        String customer1 = "Cindhuri";
+        String customer2 = "Sam";
+        String customer3 = "Jimmy";
+        String customer4 = "Noah";
+    
+        // Order completions (add yours below)
+        boolean isReadyOrder1 = false;
+        boolean isReadyOrder2 = true;
+        boolean isReadyOrder3 = false;
+        boolean isReadyOrder4 = true;
+    
+        // APP INTERACTION SIMULATION (Add your code for the challenges below)
+        // Example:
+        System.out.println(generalGreeting + customer1); // Displays "Welcome to Cafe Java, Cindhuri"
+        // ** Your customer interaction print statements will go here ** //
+        System.out.println(customer1 + pendingMessage);
+        
+        // Noah
+        if (isReadyOrder4) {
+            System.out.println(customer4 + readyMessage);
+            System.out.println(displayTotalMessage + cappucino);
+        }
+        else {
+            System.out.println(customer4 + pendingMessage);
+        }
 
+        // Sam
+        double order2 = (latte + latte);
+        System.out.println(displayTotalMessage + order2);
+        if (isReadyOrder2) {
+            System.out.println(customer2 + readyMessage);
+        }
+        else {
+            System.out.println(customer2 + pendingMessage);
+        }
+
+        // Jimmy
+        double order3 = (latte - dripCoffee);
+        System.out.println(customer3 + displayTotalMessage + order3);
     }
 }
